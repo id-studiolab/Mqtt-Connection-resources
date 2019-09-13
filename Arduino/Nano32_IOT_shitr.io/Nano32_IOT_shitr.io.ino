5c17bd7d94ca25f8179573f67e1a5e2430c9f18c
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2019-09-13 12:06:42 +0200

    add sketch to connect to shiftr.io from Arduino

---
+++ Arduino/Nano32_IOT_shitr.io/Nano32_IOT_shitr.io.ino
@@ -0,0 +1,59 @@
+#include <WiFiNINA.h>
+#include <MQTT.h>
+
+const char ssid[] = "wifi_ssif";
+const char pass[] = "wifi_psw";
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("nano", "connected-thing", "c784e41dd3da48d4")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+  // You need to set the IP address directly.
+  client.begin("broker.shiftr.io", net);
+  client.onMessage(messageReceived);
+
+  connect();
+}
+
+void loop() {
+  client.loop();
+
+  if (!client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    client.publish("/hello", "world");
+  }
+}
